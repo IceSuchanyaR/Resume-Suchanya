15d7782b969ff59e3560e3adffb60d813fe7118c
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-05-25 20:46:41 +0700

    Updates

---
+++ precache-manifest.2765baf8df2dbc6fc60e3390f6ab4ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508a55a0853fb50b9d724fcf79c82f2a",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/index.html"
+  },
+  {
+    "revision": "2829e436cbd2624fb5c2",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/static/css/main.e7ec478a.chunk.css"
+  },
+  {
+    "revision": "30d22c3a2870f941e998",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/static/js/2.40d3b21a.chunk.js"
+  },
+  {
+    "revision": "2829e436cbd2624fb5c2",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/static/js/main.e2901742.chunk.js"
+  },
+  {
+    "revision": "e41899746e8bb950c308",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/static/js/runtime~main.6e5ab0e1.js"
+  },
+  {
+    "revision": "908602cacf86466cda8a3806e18adf59",
+    "url": "/IceSuchanyaR/Resume-Suchanya.git/static/media/77386.908602ca.jpg"
+  }
+]);
